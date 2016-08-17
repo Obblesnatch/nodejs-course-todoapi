c1b4c94f04ae37211f7ea49688a8cec458484e3f
Obblesnatch <jconabree454@gmail.com>
2016-08-17 13:44:01 -0400

    Base for todo system. Testing routes

--- server.js
+++ server.js
@@ -2,8 +2,42 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+var todos = [{
+	id: 1,
+	description: 'Get the food',
+	completed: false
+},{
+	id: 2,
+	description: 'Pickup the dog from school',
+	completed: false
+},{
+	id: 3,
+	description: 'Learn to canoe',
+	completed: true
+}];
+
 app.get('/', function(req, res) {
 	res.send('Todo API Route');
+});
+
+app.get('/todos', function(req, res) {
+	res.json(todos);
+});
+
+app.get('/todos/:id', function(req, res) {
+	var matched = null;
+	var requested = parseInt(req.params.id);
+
+	todos.forEach(function(todo) {
+		if(todo.id == requested) {
+			matched = todo;
+		}
+	});
+	if(matched) {
+		res.json(matched);
+	}else {
+		res.status(404).send();
+	}
 });
 
 app.listen(PORT, function() {
